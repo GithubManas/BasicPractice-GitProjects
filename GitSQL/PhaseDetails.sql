8e31cede4d997d133e5038dee05894086ef4cf82
Manas <ms00106399@techmahindra.com>
2020-04-16 17:26:32 +0530

    NewSQL fileadded

---
+++ GitSQL/PhaseDetails.sql
@@ -0,0 +1,10 @@
+﻿create schema [config]
+go
+
+CREATE TABLE [config].[ControlPhasedetails](
+	[Phaseid] [int] NOT NULL,
+	[Phasename] [varchar](50) NOT NULL,
+	[Createdby] [sysname] NOT NULL,
+	[Createdon] [datetime] NOT NULL,
+	[switch] [nchar](1) NULL)
+
